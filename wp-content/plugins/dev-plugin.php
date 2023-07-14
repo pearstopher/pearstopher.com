6430b513a15b34437a7fc63c4c856b1c2b9dc266
Pearstopher <pearstopher@gmail.com>
2023-07-14 15:24:07 -0700

    added empty plugin and theme directory

---
+++ wp-content/plugins/dev-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Dev Plugin
+ * @version 0.01
+ */
+/*
+Plugin Name: Developer Plugin
+Plugin URI: http://github.com/pearstopher/wp-dev
+Description: This is an empty plugin.
+Author: Christopher Snow
+Version: 0.01
+Author URI: http://github.com/pearstopher/wp-dev
+*/
