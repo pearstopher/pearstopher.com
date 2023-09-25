3958cff0a414a2e4be67db5b7bbe4e05de4b80ac
Pearstopher <pearstopher@gmail.com>
2023-09-25 11:03:37 -0700

    added empty stylesheet so theme can be activated

--- wp-content/themes/dev-theme/index.php
+++ wp-content/themes/dev-theme/index.php
@@ -1,2 +1,33 @@
-<?php
-// shh
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+    <link rel="stylesheet" href="styles.css">
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+    <header>
+        <h1>Your Site Name</h1>
+        <nav>
+            <ul>
+                <li><a href="#section1">Section 1</a></li>
+                <li><a href="#section2">Section 2</a></li>
+                <!-- Add more menu items as needed -->
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <!-- Your main content goes here -->
+    </main>
+
+    <footer>
+        <!-- Your footer content goes here -->
+    </footer>
+</body>
+
+</html>
