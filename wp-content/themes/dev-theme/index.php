0bbebb948cd479d9e592e875dcc4c03393fb3721
Pearstopher <pearstopher@gmail.com>
2023-09-25 11:41:07 -0700

    updated CSS for header/footer

--- wp-content/themes/dev-theme/index.php
+++ wp-content/themes/dev-theme/index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Website Title</title>
-    <link rel="stylesheet" href="styles.css">
-    <script src="script.js" defer></script>
+    <?php wp_head(); ?>
+
 </head>
 
 <body>
