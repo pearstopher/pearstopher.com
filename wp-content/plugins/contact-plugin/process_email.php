8a2fde8b04063a2daeb8b2647a20f2224c275ac6
Pearstopher <pearstopher@gmail.com>
2023-10-02 12:42:38 -0700

    process new form fields and send discord notification

--- wp-content/plugins/contact-plugin/process_email.php
+++ wp-content/plugins/contact-plugin/process_email.php
@@ -4,20 +4,40 @@ define("WP_USE_THEMES", false);
 require_once "../../../wp-load.php";
 if (isset($_POST["maths"])) {
     $maths = $_POST["maths"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-    // For example, send it to your external URL
-    //$response = wp_remote_post("http://external-url.com/process-email", [
-    //    "body" => ["email" => $email],
-    //]);
     if ($maths == 69) {
-        echo "My email address is pearstopher@gmail.com. Talk to you soon!";
-    } else {
-        echo "Are you sure about that?";
-    }
+        // send the request do discord
+        $response = wp_remote_post(
+            "https://discord.com/api/webhooks/1156650041692409928/_FwUMLeSNvL7gr-rUtrz6DnyS3L6OrrA0-xHfAZCaVKwmA-6Hzn6tJhLd4dC9eAUWq0Q?=",
+            [
+                "headers" => ["Content-Type" => "application/json"],
+                "data_format" => "body",
+                "body" => json_encode([
+                    "username" => $email,
+                    "content" => $message,
+                ]),
+            ]
+        );
+        if (!is_wp_error($response) && $response["response"]["code"] == 204) {
+            // 200 successful
+            // 201 created successfully
+            // 204 successful, no content returned
+            echo "Discord notification sent successfully (" .
+                $response["body"] .
+                ") Your message and email address were received. ";
+        } else {
+            echo "Error: unable to send Discord notification. I'll look into that right away. In the meantime, my email is pearstopher@gmail.com.";
 
-    //     if (!is_wp_error($response) && $response["response"]["code"] == 200) {
-    //         echo $response["body"]; // Output response from external URL
-    //     } else {
-    //         echo "Error sending email.";
-    //     }
+            //             echo "Error: " .
+            //                 $response["response"]["message"] .
+            //                 ", " .
+            //                 $response["body"] .
+            //                 ", " .
+            //                 $response["response"]["code"];
+        }
+    } else {
+        echo "Your math looks a little off to me.";
+    }
 }
